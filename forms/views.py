8bfb5f87f257021faef9a3a7f9a580eaf966e5d7
munemo <john.chimbani@outlook.com>
2021-10-18 10:53:13 +0200

    adding api

--- forms/views.py
+++ forms/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from forms.models import User
+from rest_framework import viewsets, permissions
+from forms.serializers import SlottSerializer, UserSerializer, BookingSerializer
+from forms.models import User, Booking, Slott
 
-def index(request):
-    my_dict = {'insert_me': 'Hello!'}
-    return render(request, 'forms/index.html', context=my_dict)
+ 
 
-def users(request):
-    user_list = User.objects.all()
-    my_dict = {'users':user_list}
-    return render(request,'forms/forms.html',context=my_dict)
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows bookings to be viewed or edited.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+  
+class SlottViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows bookings to be viewed or edited.
+    """
+    queryset = Slott.objects.all()
+    serializer_class = SlottSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
